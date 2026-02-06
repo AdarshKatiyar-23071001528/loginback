dbab99d9ed52542fa2e9fd27d55b990e8320d415
AdarshKatiyar-23071001528 <adarshkatiyar73@gmail.com>
2026-02-06 13:55:58 +0530

    controll update

--- Controller/User.js
+++ Controller/User.js
@@ -2,9 +2,11 @@ import { User } from "../Model/User.js";
 import bcrypt from 'bcryptjs';
 export const register = async (req,res) => {
     const {name,password,email} = req.body;
+   
     try{
+        if(name === "" || email === "" || password === "") return res.json({message:"Please Fill All field",success: false});
         let user = await User.findOne({email});
-        if(user) return res.json({message:"Already registerd please login",success:false});
+        if(user) return res.json({message:"Already registerd please login1",success:false});
         const hashPass = await bcrypt.hash(password,10);
         user = await User.create({name,password:hashPass,email});
         res.json({message:"Register Successfully",success:true,user});
